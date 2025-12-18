b88c68ce1a8a660304bd5257864f47c05426ac5e
Tyler Webster <twebster137@gmail.com>
2025-12-17 18:32:21 -0600

    refactor lexer

--- lexer.rb
+++ lexer.rb
@@ -2,8 +2,30 @@ require_relative 'token'
 
 class Lexer
   WHITESPACE = [ " ", "\t", "\n", "\r", "\f", "\v" ].freeze
+  KEYWORDS = {
+    "fn" => Token::FN,
+    "let" => Token::LET,
+    "true" => Token::TRUE,
+    "false" => Token::FALSE,
+    "if" => Token::IF,
+    "else" => Token::ELSE,
+    "return" => Token::RETURN
+  }.freeze
+  SINGLE_CHAR_TOKENS = {
+    '(' => Token::LPAREN,
+    ')' => Token::RPAREN,
+    '{' => Token::LBRACE,
+    '}' => Token::RBRACE,
+    '[' => Token::LBRACK,
+    ']' => Token::RBRACK,
+    ';' => Token::SEMICOLON,
+    ',' => Token::COMMA,
+    ':' => Token::COLON
+  }.freeze
 
   private_constant :WHITESPACE
+  private_constant :KEYWORDS
+  private_constant :SINGLE_CHAR_TOKENS
 
   def initialize(input:)
     @input = input
@@ -12,13 +34,16 @@ class Lexer
 
   def get_tokens
     res = []
-
-    tok = nil
-    until ((tok = next_token).class == Token::EOF)
-      res.append(tok)
+    errors = []
+    loop do
+      tok = next_token
+      res << tok
+      errors << tok if tok.is_a?(Token::ILLEGAL)
+      break if tok.is_a?(Token::EOF)
     end
 
-    res.append(tok)
+    raise LexerError.new(errors) unless errors.empty?
+    res
   end
 
   private
@@ -31,126 +56,93 @@ class Lexer
     ch = @input[@pos]
     tok = nil
 
-    case ch
-    when '('
-      tok = Token::LPAREN.new
-      @pos += 1
-    when ')'
-      tok = Token::RPAREN.new
-      @pos += 1
-    when '['
-      tok = Token::LBRACE.new
-      @pos += 1
-    when ']'
-      tok = Token::RBRACE.new
-      @pos += 1
-    when '{'
-      tok = Token::LBRACK.new
-      @pos += 1
-    when '}'
-      tok = Token::RBRACK.new
-      @pos += 1
-    when '!'
-      if peek == '='
-        tok = Token::NE.new
-        @pos += 2
-      else
-        tok = Token::BANG.new
-        @pos += 1
-      end
-    when '='
-      if peek == '='
-        tok = Token::EQ.new
-        @pos += 2
-      else
-        tok = Token::ASSIGN.new
-        @pos += 1
-      end
-    when '<'
-      if peek == '='
-        tok = Token::LE.new
-        @pos += 2
-      else
-        tok = Token::LT.new
-        @pos += 1
-      end
-    when '>'
-      if peek == '='
-        tok = Token::GE.new
-        @pos += 2
-      else
-        tok = Token::GT.new
-        @pos += 1
-      end
-    when ';'
-      tok = Token::SEMICOLON.new
-      @pos += 1
-    when ','
-      tok = Token::COMMA.new
-      @pos += 1
-    when ':'
-      tok = Token::COLON.new
-      @pos += 1
-    when '+'
-      if peek == '+'
-        tok = Token::INC.new
-        @pos += 2
-      else
-        tok = Token::PLUS.new
-        @pos += 1
-      end
-    when '-'
-      if peek == '-'
-        tok = Token::DEC.new
-        @pos += 2
-      else
-        tok = Token::MINUS.new
-        @pos += 1
-      end
-    when '*'
-      tok = Token::ASTERISK.new
-      @pos += 1
-    when '/'
-      tok = Token::SLASH.new
+    if SINGLE_CHAR_TOKENS.key?(ch)
+      tok = SINGLE_CHAR_TOKENS[ch].new
       @pos += 1
     else
-      if is_letter(ch)
-        s = consume_id_or_kw
-        case s
-        when 'fn'
-          tok = Token::FN.new
-        when 'let'
-          tok = Token::LET.new
-        when 'true'
-          tok = Token::TRUE.new
-        when 'false'
-          tok = Token::FALSE.new
-        when 'if'
-          tok = Token::IF.new
-        when 'else'
-          tok = Token::ELSE.new
-        when 'return'
-          tok = Token::RETURN.new
+      case ch
+      when '!'
+        if peek == '='
+          tok = Token::NE.new
+          @pos += 2
         else
-          tok = Token::ID.new(s)
-        end
-      elsif is_digit(ch)
-        s = consume_digit
-        if @input[@pos] == '.'
-          s += '.'
+          tok = Token::BANG.new
           @pos += 1
-          s += consume_digit 
-          begin
-            tok = Token::FLOAT.new(Float(s))
-          rescue ArgumentError
-            tok = Token::ILLEGAL.new
+        end
+      when '='
+        if peek == '='
+          tok = Token::EQ.new
+          @pos += 2
+        else
+          tok = Token::ASSIGN.new
+          @pos += 1
+        end
+      when '<'
+        if peek == '='
+          tok = Token::LE.new
+          @pos += 2
+        else
+          tok = Token::LT.new
+          @pos += 1
+        end
+      when '>'
+        if peek == '='
+          tok = Token::GE.new
+          @pos += 2
+        else
+          tok = Token::GT.new
+          @pos += 1
+        end
+      when '+'
+        if peek == '+'
+          tok = Token::INC.new
+          @pos += 2
+        else
+          tok = Token::PLUS.new
+          @pos += 1
+        end
+      when '-'
+        if peek == '-'
+          tok = Token::DEC.new
+          @pos += 2
+        else
+          tok = Token::MINUS.new
+          @pos += 1
+        end
+      when '*'
+        tok = Token::ASTERISK.new
+        @pos += 1
+      when '/'
+        tok = Token::SLASH.new
+        @pos += 1
+      else
+        if is_letter(ch)
+          s = consume_id_or_kw
+          tok_type = KEYWORDS.fetch(s, Token::ID)
+          tok = tok_type == Token::ID ? Token::ID.new(s) : tok_type.new
+        elsif is_digit(ch)
+          s = consume_digit
+          if @input[@pos] == '.'
+            s += '.'
+            @pos += 1
+            frac = consume_digit
+            if frac == ''
+              tok = Token::ILLEGAL.new(s)
+            else
+                s << frac
+              begin
+                tok = Token::FLOAT.new(Float(s))
+              rescue ArgumentError
+                tok = Token::ILLEGAL.new(s)
+              end
+            end
+          else
+            tok = Token::INT.new(Integer(s))
           end
         else
-          tok = Token::INT.new(Integer(s))
+          tok = Token::ILLEGAL.new(ch)
+          @pos += 1
         end
-      else
-        tok = Token::ILLEGAL.new
-        @pos += 1
       end
     end
 
@@ -166,24 +158,26 @@ class Lexer
   def consume_id_or_kw
     s = ''
     while @pos < @input.length && (is_letter(@input[@pos]) || is_digit(@input[@pos]))
-      s += @input[@pos]
+      # Use StringIO?
+      s << @input[@pos]
       @pos += 1
     end
     s
   end
 
-  # tokens like 1.0abc are valid, should this be the case?
+  # tokens like 1.0abc are valid (FLOAT & ID), should this be the case?
   def consume_digit
     s = ''
     while @pos < @input.length && is_digit(@input[@pos])
-      s += @input[@pos]
+      # Use StringIO?
+      s << @input[@pos]
       @pos += 1
     end
     s
   end
 
   def peek
-    return '' if @pos + 1 >= @input.length
+    return nil if @pos + 1 >= @input.length
     return @input[@pos + 1]
   end
 
@@ -193,5 +187,15 @@ class Lexer
 
   def is_digit(ch)
     '0' <= ch && ch <= '9'
+  end
+end
+
+class LexerError < StandardError
+  attr_reader :tokens
+
+  def initialize(tokens)
+    @tokens = tokens
+    tokens_str = tokens.map(&:to_s).join(', ')
+    super("Lexer encountered illegal tokens: #{tokens_str}")
   end
 end
